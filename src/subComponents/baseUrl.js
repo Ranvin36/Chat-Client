55019ef4fb37efd62dde67584b2a8a0e440d3b16
Ranvin36 <141607057+Ranvin36@users.noreply.github.com>
2024-02-04 15:43:29 +0530

    Update baseUrl.js

--- src/subComponents/baseUrl.js
+++ src/subComponents/baseUrl.js
@@ -1,3 +1,5 @@
-const baseUrl = "https://fleexy-chat-api.onrender.com"
+// const baseUrl = "https://fleexy-chat-api.onrender.com"
+const baseUrl = "http://localhost:3001"
+
 
 export default baseUrl
