6fef0aae71f5da4e01c6b57a9b13b0e8fb524d98
Ranvin36 <141607057+Ranvin36@users.noreply.github.com>
2024-02-04 14:25:31 +0530

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -40,7 +40,7 @@ function Home(){
 
     const navigate=useNavigate()
     const dispatch = useDispatch()
-    const Socket = io("https://fleexy-chat-api.onrender.com/")
+    const Socket = io("https://fleexy-chat-api.onrender.com")
 
     console.log(baseUrl)
 
